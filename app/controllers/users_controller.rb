302ea5a0f75b6ced9036d204600e201a4536d287
blanko <eric@blanko.be>
2013-12-12 21:24:04 -0200

    Configuração da action new e criaçõa da respectiva view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
   def index
-  	render "index"
+    render "index"
   end
 
   def new
+    @user = User.new
   end
 
   def create
   end
 
-  
+
 end
