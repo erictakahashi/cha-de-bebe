da934248ed3a8e17ace08ea99aeb9e70f0557d98
blanko <eric@blanko.be>
2013-12-13 20:33:05 -0200

    Exibição do chá no show e testes; criação da fixture users; criação da action show

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -20,4 +20,17 @@ class UsersControllerTest < ActionController::TestCase
 
     assert_redirected_to user_path(assigns(:user))
   end
+
+  test "deve exibir o chá de bebe" do
+    #Fixtures users
+    user = users(:cha_de_alguem)
+
+    get :show, id: user.id
+    assert_response :success
+    assert_select '#father_name', user.father_name
+    assert_select '#mother_name', user.mother_name
+    assert_select '#child_name', user.child_name
+    assert_select '#event_date', user.event_date.to_s
+    assert_select '#message', user.message
+  end
 end
