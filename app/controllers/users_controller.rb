82f440dd3e942eebbe7bae7c77cc8ac6506b1a5c
blanko <eric@blanko.be>
2013-12-12 22:55:18 -0200

    Criação da action create e teste de criação de usuários

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,16 @@ class UsersController < ApplicationController
   end
 
   def create
+  	@user = User.new(params_user(params))
+  	@user.save
   end
 
-
+  private
+  def params_user(params)
+  	params.require(:user).permit(:father_name,
+  								:mother_name,
+  								:child_name,
+  								:event_date,
+  								:message)
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,11 +1,22 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
   test "pagina do formulario de cadastro deve abrir" do
     get :new
     assert_response :success
+  end
+
+  test "deve salvar o usuario submetido" do
+    assert_difference('User.count') do
+      post :create, user: {
+                            father_name: 'Fulano', 
+                            mother_name: 'Joana',
+                            child_name: 'Spock',
+                            event_date: Time.now,
+                            message: 'Mensagem do chá de bebe'
+                          }
+    end
+
+    #assert_redirect_to user_path(assigns(:user))
   end
 end
